095207405a5932ffd30dc9d9abaf88d4f570b8a1
aayush-sharma001 <aayush001sharma@gmail.com>
2025-05-20 23:07:19 +0530

    add: Traversal Methods for tree

--- Tree/2-tree-traversal.cpp
+++ Tree/2-tree-traversal.cpp
@@ -1,72 +1,153 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-class Node {
-    public: 
-        int data;
-        Node* left = NULL;
-        Node* right = NULL;
-    
-    Node(int d) {
-        this -> data = d;
-        this -> left = NULL;
-        this -> right = NULL;
+class Node
+{
+public:
+    int data;
+    Node *left = NULL;
+    Node *right = NULL;
+
+    Node(int d)
+    {
+        this->data = d;
+        this->left = NULL;
+        this->right = NULL;
     }
 };
 
-Node* buildTree (Node* root) {
+Node *buildTree(Node *root)
+{
     cout << "Enter the data: " << endl;
     int data;
     cin >> data;
 
     root = new Node(data);
 
-    if(data == -1) {
+    if (data == -1)
+    {
         return NULL;
     }
 
     cout << "Enter data to insert at left of " << data << endl;
-    root -> left = buildTree(root->left);
+    root->left = buildTree(root->left);
     cout << "Enter data to insert at right of " << data << endl;
-    root -> right = buildTree(root->right);
+    root->right = buildTree(root->right);
     return root;
 }
 
-//LEVEL Order
-void levelOrderTraversal(Node* root) {
-    queue<Node*> q;
+// LEVEL Order
+void levelOrderTraversal(Node *root)
+{
+    queue<Node *> q;
     q.push(root);
     q.push(NULL);
 
-    while(!q.empty()) {
-        Node* temp = q.front();
+    while (!q.empty())
+    {
+        Node *temp = q.front();
         q.pop();
 
-        if(temp == NULL) {
+        if (temp == NULL)
+        {
             cout << endl;
             // q.push(NULL);
-            if(!q.empty()) {
+            if (!q.empty())
+            {
                 q.push(NULL);
             }
-        } else {
-            cout<<temp->data<< " ";
-            if(temp->left) {
+        }
+        else
+        {
+            cout << temp->data << " ";
+            if (temp->left)
+            {
                 q.push(temp->left);
             }
-            if (temp-> right) {
+            if (temp->right)
+            {
                 q.push(temp->right);
             }
         }
     }
 }
 
-int main () {
-    Node* root = NULL;
+// INORDER Traversal
+void inorder(Node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+
+    inorder(root->left);
+    cout << root->data << " ";
+    inorder(root->right);
+}
+
+// PREORDER Traversal
+void preorder(Node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+
+    cout << root->data << " ";
+    preorder(root->left);
+    preorder(root->right);
+}
+
+// POSTORDER Traversal
+void postorder(Node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+
+    postorder(root->left);
+    postorder(root->right);
+    cout << root->data << " ";
+}
+
+// Main Logic
+void levelOrderTraversal2(Node *root)
+{
+    queue<Node *> q;
+    q.push(root);
+
+    while (!q.empty())
+    {
+        Node *temp = q.front();
+        cout << temp->data << " ";
+        q.pop();
+
+        if (temp->left)
+        {
+            q.push(temp->left);
+        }
+        if (temp->right)
+        {
+            q.push(temp->right);
+        }
+    }
+}
+
+int main()
+{
+    Node *root = NULL;
 
     root = buildTree(root);
 
-    //1 3 7 -1 -1 11 -1 -1 5 17 -1 -1 -1
+    // 1 3 7 -1 -1 11 -1 -1 5 17 -1 -1 -1
     levelOrderTraversal(root);
-
+    levelOrderTraversal2(root);
+    cout << endl;
+    inorder(root);
+    cout << endl;
+    preorder(root);
+    cout << endl;
+    postorder(root);
     return 0;
 }
